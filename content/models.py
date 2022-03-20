0742e34425c5acc1d353b20b956b64300e258b50
c0rat1e5 <cccc0rat1e5@icloud.com>
2022-03-20 18:33:17 +0900

    Subscription and Pricing

--- content/models.py
+++ content/models.py
@@ -3,6 +3,40 @@ from django.urls import reverse
 from django.utils.text import slugify
 from django.db.models.signals import pre_save
 
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+from allauth.account.signals import email_confirmed
+
+
+class Pricing(models.Model):
+    name = models.CharField(max_length=100, unique=True)  # basic pro master
+    slug = models.SlugField(unique=True, blank=True, null=True)
+    stripe_price_id = models.CharField(max_length=50)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Subscription(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    pricing = models.ForeignKey(
+        Pricing,
+        on_delete=models.CASCADE,
+        related_name="subscriptions",
+        null=True,
+        blank=True,
+    )
+    stripe_subscription_id = models.CharField(max_length=50, null=True, blank=True)
+    status = models.CharField(max_length=100, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.email
+
 
 class Course(models.Model):
     name = models.CharField(max_length=100, unique=True)
@@ -38,11 +72,21 @@ def pre_save_course(sender, instance, *args, **kwargs):
     instance.slug = slugify(instance.name, allow_unicode=True)
 
 
-pre_save.connect(pre_save_course, sender=Course)
-
-
 def pre_save_video(sender, instance, *args, **kwargs):
     instance.slug = slugify(instance.title, allow_unicode=True)
 
 
+def pre_save_pricing(sender, instance, *args, **kwargs):
+    instance.slug = slugify(instance.name, allow_unicode=True)
+
+
+def post_email_confirmed(request, email_address, *args, **kwargs):
+    user = User.objects.get(email=email_address.email)
+    subscription = Subscription.objects.create(user=user)
+    subscription.save()
+
+
+pre_save.connect(pre_save_course, sender=Course)
 pre_save.connect(pre_save_video, sender=Video)
+pre_save.connect(pre_save_pricing, sender=Pricing)
+email_confirmed.connect(post_email_confirmed)
